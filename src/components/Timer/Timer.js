56dfb090d56e0eb9223acd052119b988b97808a8
emilpio <emilpio@gmail.com>
2022-11-03 11:41:43 +0100

    fix interval

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,21 +1,24 @@
 import styles from './Timer.module.scss';
 import React, { useState, useEffect } from 'react';
 
-const Timer = () => {
+const Timer = (props) => {
   const [time, setTime] = useState(0);
-  const [running, setRunning] = useState(false);
+  const [isStopped, setIsStopped] = useState(true);
 
   useEffect(() => {
-    let interval;
-    if (running) {
+    let interval = null;
+    if (!isStopped) {
       interval = setInterval(() => {
         setTime((prevTime) => prevTime + 10);
       }, 10);
-    } else if (!running) {
+    } else {
       clearInterval(interval);
     }
-    return () => clearInterval(interval);
-  }, [running]);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isStopped]);
+
   return (
     <div className={styles.time}>
       <div className={styles.digits}>
@@ -25,10 +28,10 @@ const Timer = () => {
         <span>{('0' + ((time / 1) % 100)).slice(-2)}</span>
       </div>
       <div className='buttons'>
-        <button className={styles.button} onClick={() => setRunning(true)}>
+        <button className={styles.button} onClick={() => setIsStopped(false)}>
           Start
         </button>
-        <button className={styles.button} onClick={() => setRunning(false)}>
+        <button className={styles.button} onClick={() => setIsStopped(true)}>
           Stop
         </button>
         <button className={styles.button} onClick={() => setTime(0)}>
