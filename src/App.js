5e182b9802fb98bb2c7a868ebd052c6b1eb87fa1
emilpio <emilpio@gmail.com>
2022-11-22 11:27:39 +0100

    add fix

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
-import Timer from './components/Timer/Timer.js';
-import Button from './components/Button/Button.js';
-import Container from '../src/components/Container/Container';
-
-// const Stoper= () => {
-// const [actualTimer, setTime ] = useState(0, null);
+import Timer from './components/Timer/Timer';
+import Button from './components/Button/Button';
+import { useState, useEffect } from 'react';
+import Container from './components/Container/Container';
 
 const App = () => {
+  const [time, setTime] = useState(0);
+
+  const [timer, setTimer] = useState(null);
+
+  const start = (e) => {
+    e.preventDefault();
+    setTimer(
+      setInterval(() => {
+        setTime((prevValue) => prevValue + 1);
+      }, 1)
+    );
+  };
+
+  const stop = (e) => {
+    e.preventDefault();
+    if (timer) clearInterval(timer);
+  };
+
+  useEffect(() => {
+    return () => {
+      if (timer) clearInterval(timer);
+    };
+  }, []);
+
   return (
     <Container>
-      <div>
-        <h1>To jest Timer</h1>
-        <Timer />
-      </div>
+      <Timer time={time} />
+      <Button onClick={start}>Start</Button>
+      <Button onClick={stop}>Stop</Button>
+      <Button
+        onClick={() => {
+          setTime(0);
+          if (timer) clearInterval(timer);
+        }}>
+        Reset
+      </Button>
     </Container>
   );
 };
--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,44 +1,24 @@
 import styles from './Timer.module.scss';
-import React, { useState, useEffect } from 'react';
 
-const Timer = (props) => {
-  const [time, setTime] = useState(0);
-  const [isStopped, setIsStopped] = useState(true);
+const Timer = ({ time }) => {
+  const convertedTime = (miliseconds) => {
+    let ms = miliseconds % 1000;
+    let s = Math.floor(miliseconds / 1000) % 60;
+    let m = Math.floor(miliseconds / 1000 / 60) % 60;
+    let h = Math.floor(miliseconds / 1000 / 60 / 60);
 
-  useEffect(() => {
-    let interval = null;
-    if (!isStopped) {
-      interval = setInterval(() => {
-        setTime((prevTime) => prevTime + 10);
-      }, 10);
-    } else {
-      clearInterval(interval);
-    }
-    return () => {
-      clearInterval(interval);
-    };
-  }, [isStopped]);
+    s < 10 ? (s = `0${s}`) : (s = `${s}`);
+    m < 10 ? (m = `0${m}`) : (m = `${m}`);
+    h < 10 ? (h = `0${h}`) : (h = `${h}`);
+
+    return h + ':' + m + ':' + s + '.' + ms;
+  };
 
   return (
-    <div className={styles.time}>
-      <div className={styles.digits}>
-        <span>{('0' + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
-        <span>{('0' + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
-        <span>{('0' + ((time / 10) % 100)).slice(-2)}:</span>
-        <span>{('0' + ((time / 1) % 100)).slice(-2)}</span>
-      </div>
-      <div className='buttons'>
-        <button className={styles.button} onClick={() => setIsStopped(false)}>
-          Start
-        </button>
-        <button className={styles.button} onClick={() => setIsStopped(true)}>
-          Stop
-        </button>
-        <button className={styles.button} onClick={() => setTime(0)}>
-          Reset
-        </button>
-      </div>
+    <div>
+      <h1 className={styles.timer}>{convertedTime(time)}</h1>
     </div>
   );
 };
+
 export default Timer;
